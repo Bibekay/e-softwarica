27871966a9966778030781b85556646155576163
bibekay <bibuayadi@gmail.com>
2019-11-22 12:40:38 +0545

    complete

---
+++ app/src/main/java/com/example/esoftwarica_bibu/HomeActivity.java
@@ -0,0 +1,67 @@
+package com.example.esoftwarica_bibu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.esoftwarica_bibu.AboutUs.AboutUsFragment;
+import com.example.esoftwarica_bibu.AddStudent.AddStudentFragment;
+import com.example.esoftwarica_bibu.Home.HomeFragment;
+import com.example.esoftwarica_bibu.Model.Students;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public static List<Students> studentsList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        getSupportActionBar().hide();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+
+        studentsList = new ArrayList<>();
+        studentsList.add(new Students("Bibek Ayadi","Bhaktapur","Male",21,R.drawable.man_24dp));
+        studentsList.add(new Students("Paras shah","Kathmnadu","Male",42,R.drawable.man_24dp));
+
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectFragment = null;
+                    switch (item.getItemId()){
+
+                        case R.id.nav_home:
+                            selectFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_add:
+                            selectFragment = new AddStudentFragment();
+                            break;
+                        case R.id.nav_about:
+                            selectFragment = new AboutUsFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectFragment).commit();
+                    return true;
+                }
+            };
+}
